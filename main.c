484cc99a336bf847de46a45a3ff2b9c8feba9440
MrSavvy1 <destydesty112@gmail.com>
2024-01-19 16:36:05 -0800

    init

---
+++ main.c
@@ -0,0 +1,92 @@
+#include "monty.h"
+
+bus_t bus;
+/**
+ *
+ *
+ */
+void start_init(FILE *fd)
+{
+	bus.lifi = 1;
+	bus.arg = NULL;
+	bus.file = fd;
+	bus.content = NULL;
+}
+
+/**
+ *
+ *
+ *
+ */
+void parseArguments(int argc, char *argv[], FILE *file, bus_t *bus)
+{
+	if (argc != 2)
+	{
+		dprintf(2, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	*file = fopen(argv[1], "r");
+	if (!*file)
+	{
+		dprintf(2, "Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	start_init(*file);
+	bus->file = *file;
+}
+
+/**
+ *
+ *
+ *
+ */
+void processFile(FILE *file, stack_t **stack, bus_t *bus)
+{
+	char *content = NULL;
+	size_t size = 0;
+	size_t read_line = 1;
+	unsigned int counter = 0;
+
+	while (read_life > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus->content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, stack, counter, file, bus);
+		}
+		free(content);
+	}
+}
+
+/**
+ *
+ *
+ *
+ */
+void cleanup(FILE *file, stack_t *stack)
+{
+	free_stack(stack);
+	fclose(file);
+}
+
+/**
+ *
+ *
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	stack_t stack;
+	bus_t bus;
+
+	stack = NULL;
+	parseArguments(argc, argv, &file, &bus);
+	processFile(file, &stack, &bus);
+	cleanup(file, stack);
+	return 0;
+}
---
+++ monty.h
@@ -0,0 +1,58 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - holds nodes of a linked list
+ * @n: number of node
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t
+
+typedef struct bus_s
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+} bus_t
+
+extern bus_t bus;
+
+/**
+ * struct instruction_s - for holding monty instruction information/data
+ * @opcode: The code e.g push
+ *
+ *
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t
+
+
+void start_init(FILE *fd);
+void cleanup(FILE *file, stack_t *stack);void processFile(FILE *file, stack_t **stack, bus_t *bus);
+void parseArgument(int argc, char *argv[], FILE **file, bus_t *bus);
+int execute(char *content, stack_t **head, unsigned int counter, FILE *file, bus_t *bus);
+void free_stack(stack_t *head);
+
+void cleaning(stack_t **head, bus_t *bus);
+void executeOpcode(char *opcode, char *arg_str, stack_t **head, unsigned int counter, bus_t *bus);
+char *parseOpcode(char *content, bus_t *bus, unsigned int counter);
+
+
+
+void f_push(stack_t **head, unsigned int number);
+void f_pall(stack_t **head, unsigned int number);
+
+#endif /*MONTY_H*/
